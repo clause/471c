692d30e865909c4992e14df9a427e485a274055f
davci-udel <davci@udel.edu>
2026-02-12 13:58:07 -0500

    Implemented - L3 Semantic Analyzer and Tests for it

--- packages/L3/src/L3/check.py
+++ packages/L3/src/L3/check.py
@@ -28,38 +28,132 @@ def check_term(
     recur = partial(check_term, context=context)  # noqa: F841
 
     match term:
-        case Let():
+        case Let(bindings=bindings, body=body):
+            binding_identifiers = {identifier: None for (identifier, _) in bindings}
+            context = {**context, **binding_identifiers}
+            for _, term in bindings:
+                recur(term, context=context)
+            recur(body, context=context)
+
+        case LetRec(bindings=bindings, body=body):
+            new_context = decorate_context_for_lecrec(term, context=context)
+            for _, term in bindings:
+                recur(term, context=new_context)
+            recur(body, context=new_context)
+
+        case Reference(name=name):
+            if name not in context:
+                raise ValueError()
+
+        case Abstract(parameters=parameters, body=body):
+            local = {parameter: None for parameter in parameters}
+            recur(body, context={**context, **local})
+
+        case Apply(target=target, arguments=arguments):
+            recur(target)
+            for argument in arguments:
+                recur(argument)
+
+        case Immediate(value=value):
             pass
 
-        case LetRec():
+        case Primitive(operator=_operator, left=left, right=right):
+            recur(left)
+            recur(right)
+
+        case Branch(operator=_operator, left=left, right=right, consequent=consequent, otherwise=otherwise):
+            recur(left)
+            recur(right)
+            recur(consequent)
+            recur(otherwise)
+
+        case Allocate(count=count):
             pass
 
-        case Reference():
+        case Load(base=base, index=index):
+            recur(base)
+
+        case Store(base=base, index=index, value=value):
+            recur(base)
+            recur(value)
+
+        case Begin(effects=effects, value=value):  # pragma: no branch
+            for effect in effects:
+                recur(effect)
+            recur(value)
+
+
+def decorate_context_for_lecrec(
+    term: Term,
+    context: Context,
+) -> Context:
+    match term:
+        case Let(bindings=bindings, body=body):
+            new_context = {**context}
+            for identifier, t in bindings:
+                new_context = {**new_context, **{identifier: None}}
+                new_context = {**new_context, **decorate_context_for_lecrec(t, context=context)}
+            return {**new_context, **decorate_context_for_lecrec(body, context=context)}
+
+        case LetRec(bindings=bindings, body=body):
+            new_context = {**context}
+            for identifier, t in bindings:
+                new_context = {**new_context, **{identifier: None}}
+                new_context = {**new_context, **decorate_context_for_lecrec(t, context=context)}
+            return {**new_context, **decorate_context_for_lecrec(body, context=context)}
+
+        case Reference(name=name):
             pass
 
-        case Abstract():
+        case Abstract(parameters=parameters, body=body):
+            new_context = {**context, **{parameter: None for parameter in parameters}}
+            return {**new_context, **decorate_context_for_lecrec(body, context=context)}
+
+        case Apply(target=target, arguments=arguments):
+            new_context = {**context, **decorate_context_for_lecrec(target, context=context)}
+            for argument in arguments:
+                new_context = {**new_context, **decorate_context_for_lecrec(argument, context=context)}
+            return new_context
+
+        case Immediate(value=value):
             pass
 
-        case Apply():
+        case Primitive(operator=_operator, left=left, right=right):
+            return {
+                **context,
+                **decorate_context_for_lecrec(left, context=context),
+                **decorate_context_for_lecrec(right, context=context),
+            }
+
+        case Branch(operator=_operator, left=left, right=right, consequent=consequent, otherwise=otherwise):
+            new_context = {
+                **context,
+                **decorate_context_for_lecrec(left, context=context),
+                **decorate_context_for_lecrec(right, context=context),
+            }
+            return {
+                **new_context,
+                **decorate_context_for_lecrec(consequent, context=context),
+                **decorate_context_for_lecrec(otherwise, context=context),
+            }
+
+        case Allocate(count=count):
             pass
 
-        case Immediate():
-            pass
+        case Load(base=base, index=index):
+            return {**context, **decorate_context_for_lecrec(base, context=context)}
 
-        case Primitive():
-            pass
+        case Store(base=base, index=index, value=value):
+            return {
+                **context,
+                **decorate_context_for_lecrec(base, context=context),
+                **decorate_context_for_lecrec(value, context=context),
+            }
 
-        case Branch():
-            pass
+        case Begin(effects=effects, value=value):  # pragma: no branch
+            new_context = {**context}
+            for effect in effects:
+                new_context = {**new_context, **decorate_context_for_lecrec(effect, context=context)}
+            return {**new_context, **decorate_context_for_lecrec(value, context=context)}
 
-        case Allocate():
-            pass
-
-        case Load():
-            pass
-
-        case Store():
-            pass
-
-        case Begin():  # pragma: no branch
-            pass
+    return context
--- packages/L3/test/L3/test_check.py
+++ packages/L3/test/L3/test_check.py
@@ -1,24 +1,465 @@
 import pytest
 from L3.check import Context, check_term
-from L3.syntax import Reference
+from L3.syntax import (
+    Abstract,
+    Allocate,
+    Begin,
+    Apply,
+    Branch,
+    Immediate,
+    Let,
+    LetRec,
+    Load,
+    Primitive,
+    Reference,
+    Store,
+)
 
 
-@pytest.mark.skip()
-def test_check_reference_bound():
-    term = Reference(name="x")
+def test_check_let():
+    term = Let(bindings=[("x", Immediate(value=0))], body=Reference(name="x"))
+    context: Context = {}
+    check_term(term, context)
 
+    term_sibling_reference = Let(
+        bindings=[("x", Immediate(value=0)), ("y", Reference(name="x"))], body=Reference(name="y")
+    )
+    check_term(term_sibling_reference, context)
+
+    term_context_reference = Let(
+        bindings=[("a", Reference(name="x")), ("b", Reference(name="y"))], body=Reference(name="z")
+    )
+    context: Context = {"x": None, "y": None, "z": None}
+    check_term(term_context_reference, context)
+
+
+def test_check_let_fail():
+    term_body_fail = Let(bindings=[("a", Immediate(value=0))], body=Reference(name="x"))
+    context: Context = {}
+    with pytest.raises(ValueError):
+        check_term(term_body_fail, context)
+
+    term_binding_fail = Let(bindings=[("a", Reference(name="x"))], body=Immediate(value=0))
+    context: Context = {}
+    with pytest.raises(ValueError):
+        check_term(term_binding_fail, context)
+
+    term_sibling_child_reference_fail = Let(
+        bindings=[
+            ("x", Reference(name="a")),
+            ("y", Let(bindings=[("a", Immediate(value=0)), ("b", Reference(name="x"))], body=Reference(name="x"))),
+        ],
+        body=Reference(name="b"),
+    )
+    with pytest.raises(ValueError):
+        check_term(term_sibling_child_reference_fail, context)
+
+
+def test_check_letrec():
+    term = LetRec(bindings=[("x", Immediate(value=0))], body=Reference(name="x"))
+    context: Context = {}
+    check_term(term, context)
+
+    term_sibling_reference = LetRec(
+        bindings=[("x", Immediate(value=0)), ("y", Reference(name="x"))], body=Reference(name="y")
+    )
+    check_term(term_sibling_reference, context)
+
+    term_sibling_child_reference = LetRec(
+        bindings=[
+            ("x", Reference(name="a")),
+            ("y", Let(bindings=[("a", Immediate(value=0)), ("b", Reference(name="x"))], body=Reference(name="x"))),
+        ],
+        body=Reference(name="b"),
+    )
+    check_term(term_sibling_child_reference, context)
+
+    term_context_reference = LetRec(
+        bindings=[("a", Reference(name="x")), ("b", Reference(name="y"))], body=Reference(name="z")
+    )
+    context: Context = {"x": None, "y": None, "z": None}
+    check_term(term_context_reference, context)
+
+
+def test_check_letrec_fail():
+    term_body_fail = LetRec(bindings=[("a", Immediate(value=0))], body=Reference(name="x"))
+    context: Context = {}
+    with pytest.raises(ValueError):
+        check_term(term_body_fail, context)
+
+    term_binding_fail = LetRec(bindings=[("a", Reference(name="x"))], body=Immediate(value=0))
+    context: Context = {}
+    with pytest.raises(ValueError):
+        check_term(term_binding_fail, context)
+
+
+def test_let_sum():
+    term = Let(
+        bindings=[
+            (
+                "make_adder",
+                Abstract(
+                    parameters=["x"],
+                    body=Abstract(
+                        parameters=["y"],
+                        body=Primitive(operator="+", left=Reference(name="x"), right=Reference(name="y")),
+                    ),
+                ),
+            )
+        ],
+        body=Let(
+            bindings=[("adder", Apply(target=Reference(name="make_adder"), arguments=[Reference(name="m")]))],
+            body=Apply(target=Reference(name="adder"), arguments=[Reference(name="n")]),
+        ),
+    )
     context: Context = {
-        "x": None,
+        "m": None,
+        "n": None,
     }
 
     check_term(term, context)
 
 
-@pytest.mark.skip()
-def test_check_reference_free():
-    term = Reference(name="x")
+def test_letrec_fibonacci():
+    term = LetRec(
+        bindings=[
+            (
+                "fib",
+                Abstract(
+                    parameters=["n"],
+                    body=Branch(
+                        operator="<",
+                        left=Reference(name="n"),
+                        right=Immediate(value=2),
+                        consequent=Reference(name="n"),
+                        otherwise=Primitive(
+                            operator="+",
+                            left=Apply(
+                                target=Reference(name="fib"),
+                                arguments=[Primitive(operator="-", left=Reference(name="n"), right=Immediate(value=1))],
+                            ),
+                            right=Apply(
+                                target=Reference(name="fib"),
+                                arguments=[Primitive(operator="-", left=Reference(name="n"), right=Immediate(value=2))],
+                            ),
+                        ),
+                    ),
+                ),
+            )
+        ],
+        body=Apply(target=Reference(name="fib"), arguments=[Reference(name="n")]),
+    )
 
-    context: Context = {}
+    context: Context = {
+        "n": None,
+    }
 
+    check_term(term, context)
+
+
+def test_letrec_sum():
+    letrec = LetRec(
+        bindings=[
+            (
+                "loop",
+                Abstract(
+                    parameters=[],
+                    body=Branch(
+                        operator="<",
+                        left=Load(base=Reference(name="i"), index=0),
+                        right=Primitive(operator="+", left=Reference(name="n"), right=Immediate(value=1)),
+                        consequent=Begin(
+                            effects=[
+                                Store(
+                                    base=Reference(name="acc"),
+                                    index=0,
+                                    value=Primitive(
+                                        operator="+",
+                                        left=Load(base=Reference(name="acc"), index=0),
+                                        right=Load(base=Reference(name="i"), index=0),
+                                    ),
+                                ),
+                                Store(
+                                    base=Reference(name="acc"),
+                                    index=0,
+                                    value=Primitive(
+                                        operator="+",
+                                        left=Load(base=Reference(name="i"), index=0),
+                                        right=Immediate(value=1),
+                                    ),
+                                ),
+                            ],
+                            value=Apply(target=Reference(name="loop"), arguments=[]),
+                        ),
+                        otherwise=Load(base=Reference(name="acc"), index=0),
+                    ),
+                ),
+            )
+        ],
+        body=Apply(target=Reference(name="loop"), arguments=[]),
+    )
+    term = Let(
+        bindings=[("i", Allocate(count=1)), ("acc", Allocate(count=1))],
+        body=Begin(
+            effects=[
+                Store(base=Reference(name="i"), index=0, value=Immediate(value=0)),
+                Store(base=Reference(name="acc"), index=0, value=Immediate(value=0)),
+            ],
+            value=letrec,
+        ),
+    )
+
+    context: Context = {
+        "n": None,
+    }
+
+    check_term(term, context)
+
+
+def test_let_and_letrec_against_recursive_sum():
+    mock_bindings = [
+        (
+            (
+                "adder",
+                Apply(
+                    target=Reference(name="make_adder"),
+                    arguments=[
+                        Reference(name="m"),
+                        Let(
+                            bindings=[
+                                (
+                                    "make_adder",
+                                    Abstract(
+                                        parameters=["x"],
+                                        body=Abstract(
+                                            parameters=["y"],
+                                            body=Primitive(
+                                                operator="+", left=Reference(name="x"), right=Reference(name="y")
+                                            ),
+                                        ),
+                                    ),
+                                )
+                            ],
+                            body=Immediate(value=1),
+                        ),
+                    ],
+                ),
+            )
+        ),
+    ]
+    mock_body = Apply(target=Reference(name="adder"), arguments=[Reference(name="n")])
+
+    context: Context = {"m": None, "n": None}
+    term = Let(bindings=mock_bindings, body=mock_body)
     with pytest.raises(ValueError):
         check_term(term, context)
+
+    term = LetRec(bindings=mock_bindings, body=mock_body)
+    check_term(term, context)
+
+
+def test_check_reference():
+    term = Reference(name="x")
+    context: Context = {
+        "x": None,
+    }
+    check_term(term, context)
+
+
+def test_check_reference_fail():
+    term = Reference(name="x")
+    context: Context = {}
+    with pytest.raises(ValueError):
+        check_term(term, context)
+
+
+def test_check_abstract():
+    term = Abstract(parameters=["x"], body=Reference(name="x"))
+    context: Context = {}
+    check_term(term, context)
+
+    term_no_param = Abstract(parameters=[], body=Reference(name="x"))
+    context: Context = {"x": None}
+    check_term(term_no_param, context)
+
+
+def test_check_abstract_fail():
+    term = Abstract(parameters=["x"], body=Reference(name="y"))
+
+    context: Context = {}
+    with pytest.raises(ValueError):
+        check_term(term, context)
+
+
+def test_check_apply():
+    term = Apply(target=Reference(name="x"), arguments=[Reference(name="y"), Reference(name="x")])
+    context: Context = {"x": None, "y": None}
+    check_term(term, context)
+
+
+def test_check_apply_fail():
+    term_both_fail = Apply(target=Reference(name="x"), arguments=[Reference(name="y"), Reference(name="x")])
+    context: Context = {}
+    with pytest.raises(ValueError):
+        check_term(term_both_fail, context)
+
+    term_argument_fail = Apply(target=Reference(name="x"), arguments=[Reference(name="y"), Reference(name="x")])
+    context: Context = {"x": None}
+    with pytest.raises(ValueError):
+        check_term(term_argument_fail, context)
+
+    term_target_fail = Apply(target=Reference(name="z"), arguments=[Reference(name="x")])
+    context: Context = {"x": None}
+    with pytest.raises(ValueError):
+        check_term(term_target_fail, context)
+
+
+def test_check_immediate():
+    term = Immediate(value=0)
+    context: Context = {}
+    check_term(term, context)
+
+
+def test_check_primitive():
+    term = Primitive(operator="+", left=Reference(name="x"), right=Reference(name="y"))
+    context: Context = {"x": None, "y": None}
+    check_term(term, context)
+
+    term = Primitive(operator="*", left=Reference(name="x"), right=Reference(name="y"))
+    check_term(term, context)
+
+    term = Primitive(operator="-", left=Reference(name="x"), right=Reference(name="y"))
+    check_term(term, context)
+
+
+def test_check_primitive_fail():
+    term_left_fail = Primitive(operator="+", left=Reference(name="a"), right=Reference(name="y"))
+    context: Context = {"x": None, "y": None}
+    with pytest.raises(ValueError):
+        check_term(term_left_fail, context)
+
+    term_right_fail = Primitive(operator="+", left=Reference(name="x"), right=Reference(name="a"))
+    with pytest.raises(ValueError):
+        check_term(term_right_fail, context)
+
+
+def test_check_branch():
+    term = Branch(
+        operator="<",
+        left=Reference(name="x"),
+        right=Reference(name="y"),
+        consequent=Reference(name="z"),
+        otherwise=Reference(name="w"),
+    )
+    context: Context = {"x": None, "y": None, "z": None, "w": None}
+    check_term(term, context)
+
+    term = Branch(
+        operator="==",
+        left=Reference(name="x"),
+        right=Reference(name="y"),
+        consequent=Reference(name="z"),
+        otherwise=Reference(name="w"),
+    )
+    check_term(term, context)
+
+
+def test_check_branch_fail():
+    term_left_fail = Branch(
+        operator="<",
+        left=Reference(name="a"),
+        right=Reference(name="y"),
+        consequent=Reference(name="z"),
+        otherwise=Reference(name="w"),
+    )
+    context: Context = {"x": None, "y": None, "z": None, "w": None}
+    with pytest.raises(ValueError):
+        check_term(term_left_fail, context)
+
+    term_right_fail = Branch(
+        operator="<",
+        left=Reference(name="x"),
+        right=Reference(name="a"),
+        consequent=Reference(name="z"),
+        otherwise=Reference(name="w"),
+    )
+    with pytest.raises(ValueError):
+        check_term(term_right_fail, context)
+
+    term_consequent_fail = Branch(
+        operator="<",
+        left=Reference(name="x"),
+        right=Reference(name="y"),
+        consequent=Reference(name="a"),
+        otherwise=Reference(name="w"),
+    )
+    with pytest.raises(ValueError):
+        check_term(term_consequent_fail, context)
+
+    term_otherwise_fail = Branch(
+        operator="<",
+        left=Reference(name="x"),
+        right=Reference(name="y"),
+        consequent=Reference(name="z"),
+        otherwise=Reference(name="a"),
+    )
+    with pytest.raises(ValueError):
+        check_term(term_otherwise_fail, context)
+
+
+def test_check_allocate():
+    term = Allocate(count=0)
+    context: Context = {}
+    check_term(term, context)
+
+    term = LetRec(bindings=[("x", Allocate(count=0))], body=Reference(name="x"))
+    context: Context = {}
+    check_term(term, context)
+
+
+def test_check_load():
+    term = Load(base=Reference(name="x"), index=0)
+    context: Context = {"x": None}
+    check_term(term, context)
+
+
+def test_check_load_fail():
+    term = Load(base=Reference(name="a"), index=0)
+    context: Context = {"x": None}
+    with pytest.raises(ValueError):
+        check_term(term, context)
+
+
+def test_check_store():
+    term = Store(base=Reference(name="x"), index=0, value=Reference(name="y"))
+    context: Context = {"x": None, "y": None}
+    check_term(term, context)
+
+
+def test_check_store_fail():
+    term_base_fail = Store(base=Reference(name="a"), index=0, value=Reference(name="y"))
+    context: Context = {"x": None, "y": None}
+    with pytest.raises(ValueError):
+        check_term(term_base_fail, context)
+
+    term_value_fail = Store(base=Reference(name="x"), index=0, value=Reference(name="a"))
+    with pytest.raises(ValueError):
+        check_term(term_value_fail, context)
+
+
+def test_check_begin():
+    term = Begin(effects=[Reference(name="x"), Reference(name="y")], value=Reference(name="z"))
+    context: Context = {"x": None, "y": None, "z": None}
+    check_term(term, context)
+
+
+def test_check_begin_fail():
+    term_effects_fail = Begin(effects=[Reference(name="x"), Reference(name="a")], value=Reference(name="z"))
+    context: Context = {"x": None, "y": None, "z": None}
+    with pytest.raises(ValueError):
+        check_term(term_effects_fail, context)
+
+    term_value_fail = Begin(effects=[Reference(name="x"), Reference(name="y")], value=Reference(name="a"))
+    context: Context = {"x": None, "y": None, "z": None}
+    with pytest.raises(ValueError):
+        check_term(term_value_fail, context)
