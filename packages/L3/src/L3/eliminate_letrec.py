84f389840367439d0fb45659ce96a4bb29bb0632
nolanKeefe <npkeefe@udel.edu>
2026-02-18 14:35:50 -0500

    made files for in class translation purposes/reference purposes

---
+++ packages/L3/src/L3/eliminate_letrec.py
@@ -0,0 +1,7 @@
+# made for later use in L3 to convert into L2
+# based on how it looks from him is its like the check.py but with l3.the case name
+# and then some basic conversion stuff of l2. instead of a check for duplicates and stuff like that
+# returns on things like branch is return L2.Branch (all the thing in branch = recur(thing in branch))
+# L3.Reference(name = name) becomes
+# if name is a recursive variable -> Load(reference name)))
+# else return Reference(name = name)
