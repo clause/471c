8a9bf468b5f06b4840c1c2a9bf88141d16ca0da1
nolanKeefe <npkeefe@udel.edu>
2026-02-18 20:30:03 -0500

    fact.l2 made and more info added to eliminate letrec

--- packages/L3/src/L3/eliminate_letrec.py
+++ packages/L3/src/L3/eliminate_letrec.py
@@ -1,4 +1,6 @@
 # made for later use in L3 to convert into L2
+# from L2 import syntax as L2
+# from . import syntax as L3
 # based on how it looks from him is its like the check.py but with l3.the case name
 # and then some basic conversion stuff of l2. instead of a check for duplicates and stuff like that
 # returns on things like branch is return L2.Branch (all the thing in branch = recur(thing in branch))
