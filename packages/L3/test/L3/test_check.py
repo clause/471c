cb52ce4fa7f23f9f59ee86bdb3a3b89bed3a68aa
Alex M <menaalex8@gmail.com>
2026-03-16 11:56:35 -0400

    fixed a merge conflict on test check file

--- packages/L3/test/L3/test_check.py
+++ packages/L3/test/L3/test_check.py
@@ -17,10 +17,6 @@ from L3.syntax import (
 )
 
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 1e10c62 (completed semantic analysis)
 def test_check_term_let():
     term = Let(
         bindings=[
@@ -34,10 +30,6 @@ def test_check_term_let():
     check_term(term, context)
 
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 1e10c62 (completed semantic analysis)
 def test_check_term_let_scope():
     term = Let(
         bindings=[
@@ -53,10 +45,6 @@ def test_check_term_let_scope():
         check_term(term, context)
 
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 1e10c62 (completed semantic analysis)
 def test_check_term_let_duplicate_binders():
     term = Let(
         bindings=[
@@ -85,10 +73,6 @@ def test_check_term_letrec():
     check_term(term, context)
 
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 1e10c62 (completed semantic analysis)
 def test_check_term_letrec_scope():
     term = LetRec(
         bindings=[
@@ -103,10 +87,6 @@ def test_check_term_letrec_scope():
     check_term(term, context)
 
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 1e10c62 (completed semantic analysis)
 def test_check_term_letrec_duplicate_binders():
     term = LetRec(
         bindings=[
@@ -132,10 +112,6 @@ def test_check_term_reference_bound():
     check_term(term, context)
 
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 1e10c62 (completed semantic analysis)
 def test_check_term_reference_free():
     term = Reference(name="x")
 
@@ -145,10 +121,6 @@ def test_check_term_reference_free():
         check_term(term, context)
 
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 1e10c62 (completed semantic analysis)
 def test_check_term_abstract():
     term = Abstract(
         parameters=["x"],
@@ -160,10 +132,6 @@ def test_check_term_abstract():
     check_term(term, context)
 
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 1e10c62 (completed semantic analysis)
 def test_check_term_abstract_duplicate_parameters():
     term = Abstract(
         parameters=["x", "x"],
@@ -176,10 +144,6 @@ def test_check_term_abstract_duplicate_parameters():
         check_term(term, context)
 
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 1e10c62 (completed semantic analysis)
 def test_check_term_apply():
     term = Apply(
         target=Reference(name="x"),
@@ -193,10 +157,6 @@ def test_check_term_apply():
     check_term(term, context)
 
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 1e10c62 (completed semantic analysis)
 def test_check_term_immediate():
     term = Immediate(value=0)
 
@@ -205,10 +165,6 @@ def test_check_term_immediate():
     check_term(term, context)
 
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 1e10c62 (completed semantic analysis)
 def test_check_term_primitive():
     term = Primitive(
         operator="+",
@@ -221,10 +177,6 @@ def test_check_term_primitive():
     check_term(term, context)
 
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 1e10c62 (completed semantic analysis)
 def test_check_term_branch():
     term = Branch(
         operator="<",
@@ -239,10 +191,6 @@ def test_check_term_branch():
     check_term(term, context)
 
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 1e10c62 (completed semantic analysis)
 def test_check_term_allocate():
     term = Allocate(count=0)
 
@@ -251,10 +199,6 @@ def test_check_term_allocate():
     check_term(term, context)
 
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 1e10c62 (completed semantic analysis)
 def test_check_term_load():
     term = Load(
         base=Reference(name="x"),
@@ -268,10 +212,6 @@ def test_check_term_load():
     check_term(term, context)
 
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 1e10c62 (completed semantic analysis)
 def test_check_term_store():
     term = Store(
         base=Reference(name="x"),
@@ -286,10 +226,6 @@ def test_check_term_store():
     check_term(term, context)
 
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 1e10c62 (completed semantic analysis)
 def test_check_term_begin():
     term = Begin(
         effects=[Immediate(value=0)],
@@ -301,10 +237,6 @@ def test_check_term_begin():
     check_term(term, context)
 
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 1e10c62 (completed semantic analysis)
 def test_check_program():
     program = Program(
         parameters=[],
